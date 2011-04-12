4803fabdc3c244653b947d0c66560a8c22075f21
Scott Reis <scott.reis@vertive.com>
2011-04-12 09:42:54 -0500

    First commit

---
+++ filtration.gemspec
@@ -0,0 +1,9 @@
+spec = Gem::Specification.new do |s|
+  s.name = "filtration"
+  s.version = "0.0.1"
+  s.author = "R. Scott Reis"
+  s.platform = Gem::Platform::RUBY
+  s.summary = "Filtration pre/post method callback"
+  s.has_rdoc = false
+  s.files = %w[filtration.rb]
+end
---
+++ filtration.rb
@@ -0,0 +1,19 @@
+module Filtration
+
+  def prefilter(method,&block)
+    old_method = instance_method(method)
+    raise "Method #{method} takes 0 arguments" if old_method.arity == 0
+    define_method(method) do |*args|
+      old_method.bind(self).call(block.call(*args))
+    end
+  end
+
+  def postfilter(method,&block)
+    old_method = instance_method(method)
+    raise "Method #{method} takes 0 arguments" if old_method.arity == 0
+    define_method(method) do |*args|
+      block.call(old_method.bind(self).call(*args))
+    end
+  end
+
+end
