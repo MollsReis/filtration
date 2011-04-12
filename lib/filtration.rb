db782bb47f1e0c02c1dc6f5c29f17e206b715a85
Scott Reis <scott.reis@vertive.com>
2011-04-12 16:13:40 -0500

    Allow filter method to be passed instead of code block, and update test for new functionality and parameter checking

--- lib/filtration.rb
+++ lib/filtration.rb
@@ -1,18 +1,44 @@
 module Filtration
 
-  def prefilter(method,&block)
+  def prefilter(method, filter=nil, &block)
     old_method = instance_method(method)
     raise "Method #{method} takes 0 arguments" if old_method.arity == 0
+
+    if !filter.nil?
+      filter_method = instance_method(filter)
+      raise "Filter method #{filter} takes 0 arguments" if filter_method.arity == 0
+    end
+
+    raise "Cannot use both filter method and block" if !filter.nil? && !block.nil?
+    raise "Block or filter method missing" if filter.nil? && block.nil?
+
     define_method(method) do |*args|
-      old_method.bind(self).call(block.call(*args))
+      if filter.nil?
+        old_method.bind(self).call(block.call(*args))
+      else
+        old_method.bind(self).call(filter(*args))
+      end
     end
   end
 
-  def postfilter(method,&block)
+  def postfilter(method, filter=nil, &block)
     old_method = instance_method(method)
     raise "Method #{method} takes 0 arguments" if old_method.arity == 0
+
+    if !filter.nil?
+      filter_method = instance_method(filter)
+      raise "Filter method #{filter} takes 0 arguments" if filter_method.arity == 0
+    end
+
+    raise "Cannot use both filter method and block" if !filter.nil? && !block.nil?
+    raise "Block or filter method missing" if filter.nil? && block.nil?
+
     define_method(method) do |*args|
-      block.call(old_method.bind(self).call(*args))
+      if filter.nil?
+        block.call(old_method.bind(self).call(*args))
+      else
+        filter(old_method.bind(self).call(*args))
+      end
     end
   end
 
--- spec/filtration_spec.rb
+++ spec/filtration_spec.rb
@@ -1,40 +1,87 @@
 require 'spec_helper'
 
+class Good
+  def foo(x)
+    x + 2
+  end
+end
+
+class Bad
+  def foo
+    2
+  end
+end
+
 describe Filtration do
 
   it 'executes a code block before the specified method' do
-    class Foo1
-      def foo(x)
-        x + 2
-      end
-    end
-    class Test1 < Foo1
+    class Test1 < Good 
       prefilter(:foo){|x| x * 2}
     end
     Test1.new.foo(2).should === 6
   end
 
   it 'executes a code block after the specified method' do
-    class Foo2
-      def foo(x)
-        x + 2
-      end
-    end
-    class Test2 < Foo2
+    class Test2 < Good
       postfilter(:foo){|x| x * 2}
     end
     Test2.new.foo(2).should === 8
   end
 
-  it 'raises an error if the method has no arguments' do
-    class Foo3
-      def foo
-        2
+  it 'executes a code block before the specified method' do
+    class Test3 < Good 
+      def bar(x)
+        x * 2
       end
+      prefilter :foo, :bar
     end
-    class Test3 < Foo3
+    Test1.new.foo(2).should === 6
+  end
+
+  it 'executes a code block after the specified method' do
+    class Test4 < Good
+      def bar(x)
+        x * 2
+      end
+      postfilter :foo, :bar
+    end
+    Test2.new.foo(2).should === 8
+  end
+
+  it 'raises an error if the method has no arguments' do
+    class Test5 < Bad
       extend RSpec::Matchers
       lambda { prefilter(:foo){|x| x * 2} }.should raise_error
+    end
+  end
+
+  it 'raises an error if the filter method has no arguments' do
+    class Test6 < Good
+      extend RSpec::Matchers
+      def bar
+        nil
+      end
+      lambda { prefilter :foo, :bar }.should raise_error
+    end
+  end
+
+  it 'raises an error if both filter and block are specified' do
+    class Test7 < Good
+      extend RSpec::Matchers
+      def bar(x)
+        x * 2
+      end
+      lambda { prefilter(:foo,:bar){|x| x * 2} }.should raise_error
+    end
+  end
+
+  it 'raises an error if neither filter nor block are specified' do
+    class Test8 < Good
+      extend RSpec::Matchers
+      def bar(x)
+        x * 2
+      end
+      lambda { prefilter(:foo) }.should raise_error
     end
   end
 
